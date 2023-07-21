52fd69e1a19a3e1ef3ed8950765737fad5865a10
Vasco Sousa <35708844+vsousa14@users.noreply.github.com>
2023-07-21 22:06:05 +0100

    Load links working

--- js/loadLinks.js
+++ js/loadLinks.js
@@ -1,11 +1,36 @@
+function validateLink(string) {
+    if(/(http(s?)):\/\//i.test(string)) {
+      return true;
+    }else{
+        return false;
+    }
+  }
+
 function loadSavedLinks(){
     let linksContainer = document.getElementById("linksArea");
-    let links = eval(localStorage.getItem("links"));
+    let links = eval(JSON.parse(localStorage.getItem("links")));
+    let addLink = document.getElementById("openNewLinkDialog");
 
-    console.log(links)
-    console.log(links.length);
-    for(let l = 0; l <= links.length-1; l++){
-        
+    if(!links){
+        return;
+    }
+
+   console.log(links);
+    for(let l = 0; l<=links.length-1; l++){
+        //console.log(links[l]);
+        //Create HTML to load all links
+        let linkWrapper = document.createElement("a");
+        linkWrapper.href = validateLink(links[l].url) ? `${links[l].url}` : `https://${links[l].url}`;
+        linkWrapper.className = "link-container";
+        linksContainer.insertBefore(linkWrapper,addLink);
+
+        let favico = document.createElement("img");
+        favico.dataset.link = links[l].url;
+        linkWrapper.appendChild(favico);
+
+        let linkLabel = document.createElement("span");
+        linkLabel.innerText = links[l].text;
+        linkWrapper.appendChild(linkLabel);
     }
 
 }
