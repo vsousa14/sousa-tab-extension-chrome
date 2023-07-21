7225fe24fc8d0a3a7dee17496a4ac7cd74bfa57f
Vasco Sousa <35708844+vsousa14@users.noreply.github.com>
2023-07-21 22:19:33 +0100

    starting imaage categories feature

--- js/setup.js
+++ js/setup.js
@@ -1,9 +1,20 @@
+var categories = [];
+
+function addCategory(){
+
+}
+
+
 function setup(){
     let name = document.getElementById("user").value;
 
     if(!name){
         alert("Your name can't be blank");
         return;
+    }
+
+    if(categories.length <= 0){
+        categories = ["nature","mountains","city","beach","winter","summer","animals"];
     }
 
     localStorage.setItem("username",name);
