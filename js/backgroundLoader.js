cd55c110b84bb11acd8b238e52a617fde76a46dc
Vasco Sousa <35708844+vsousa14@users.noreply.github.com>
2023-07-23 12:44:40 +0100

    Change bg after 1 day

--- js/backgroundLoader.js
+++ js/backgroundLoader.js
@@ -1,9 +1,19 @@
-function renderItem(category){
+function renderItem(category, change){
     console.log("Category: ", category)
+if(change){
     fetch(`https://source.unsplash.com/${window.screen.width}x${window.screen.height}/?${category}`).then((response) => {   
+        localStorage.setItem("currentImg", response.url);
         document.body.style.backgroundImage = `url(${response.url})`; 
         document.body.style.backgroundPosition = `center`; 
         document.body.style.backgroundRepeat = `no-repeat`; 
         document.body.style.backgroundSize = `cover`; 
+
     }) 
+}else{
+     document.body.style.backgroundImage = `url(${localStorage.getItem("currentImg")})`; 
+        document.body.style.backgroundPosition = `center`; 
+        document.body.style.backgroundRepeat = `no-repeat`; 
+        document.body.style.backgroundSize = `cover`; 
+}
+    
   }
